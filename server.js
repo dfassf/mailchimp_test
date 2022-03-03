6a0d523b093438a44607cf14f5e6f322f2df6252
Shinwoo Um <dfassf@gmail.com>
2022-03-03 14:34:47 +0900

    0303

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+const mailchimp = require('@mailchimp/mailchimp_marketing')
+
 app.listen(3000,()=>{
     console.log('on port 3000')
 })
